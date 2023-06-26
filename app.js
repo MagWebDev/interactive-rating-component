346d8419077048a314efeced0dccef82f94bb5d9
MagWeb <84671218+MagWebDev@users.noreply.github.com>
2023-06-26 14:10:57 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const ratingBtns = document.querySelectorAll('.btn');
+const choiceBox = document.getElementById("userChoice");
+const submitBtn = document.getElementById("submit");
+const thxPage = document.getElementById('thankyou-page');
+const mainPage = document.getElementById('main-page');
+console.log(choiceBox)
+
+let getNum = function(e){
+   choiceBox.innerText = e.target.dataset.num;
+       
+}
+
+ratingBtns.forEach((btn)=>{
+    btn.addEventListener('click',getNum );
+    
+})
+
+submitBtn.addEventListener('click', function(){
+    thxPage.style.display = 'block';
+    mainPage.style.display= "none";
+} )
